f1232fe1de6d804b38a8fdf39afbb0a48b5be274
cedar-ma <103262367+cedar-ma@users.noreply.github.com>
2023-03-22 12:53:52 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -6,11 +6,12 @@ import nbconvert
 import numpy as np
 import pandas as pd
 import pyvista as pv
+import matplotlib.pyplot as plt
 # Convert the assignment Jupyter notebooks into something we can import
 
 
 # Import the converted file. Best to import only the variables you want to test
-from project1 import aperture_statistics, aperture_mean, roughness_coeff, tortuosity, perm
+from project1_2 import aperture_statistics, aperture_mean, roughness_coeff, tortuosity, perm
 
 # Here is where you will write your tests
 class TestSolution(unittest.TestCase):
@@ -19,14 +20,14 @@ class TestSolution(unittest.TestCase):
 
     def setUp(self):
         np.random.seed(0)
-        aperture = np.random.randint(0,150,size=(100,100))
+        self.aperture = np.random.randint(0,150,size=(100,100))
         pass
 
     # All tests need to start with "test_..."
     def test_q1(self):
         # Numpy has an extensive test suite.
         # It's best to use allclose instead of equal to avoid problems with machine precision
-        np.testing.assert_allclose(aperture_statistics(aperture), (0,149))
+        np.testing.assert_allclose(aperture_statistics(self.aperture), (0,149))
         return
 
 
@@ -34,24 +35,24 @@ class TestSolution(unittest.TestCase):
         # It also works if testing an array
         # You can write the solution yourself as follows. But students will be able to see your solution
        
-        np.testing.assert_allclose(aperture_mean(aperture), 74.5306)
+        np.testing.assert_allclose(aperture_mean(self.aperture), 74.5306)
         return
         
     def test_q3(self):
         # It also works if testing an array
         # You can write the solution yourself as follows. But students will be able to see your solution
        
-        np.testing.assert_allclose(roughness_coeff(aperture), 61.319589039718785)
+        np.testing.assert_allclose(roughness_coeff(self.aperture), 61.319589039718785)
         return
 
         
     def test_q4(self):
         # Instead, it's better to hardcode the solution if possible
-        np.testing.assert_allclose(tortuosity(aperture), 49.970110174196435)
+        np.testing.assert_allclose(tortuosity(self.aperture), 49.970110174196435)
         return
         
     def test_q5(self):
-        np.testing.assert_allclose(perm(aperture), 462.90086136333343)
+        np.testing.assert_allclose(perm(self.aperture), 462.90086136333343)
         return
         
 
